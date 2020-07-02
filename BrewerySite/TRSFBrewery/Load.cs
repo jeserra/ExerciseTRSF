92c617a85df74b9b543bdbe3704254560d9f6978
Jose Eduardo Serrano Dominguez <joser_isi@hotmail.com>
2020-07-02 10:01:08 -0500

    Update Load.cs
    Interfaces deben comenzar su nombre con I por convencion. Debe ser ILoad.

--- BrewerySite/TRSFBrewery/Load.cs
+++ BrewerySite/TRSFBrewery/Load.cs
@@ -1,6 +1,7 @@
 using System;
 namespace TRSFBrewery
 {
+    // Cambiar el nombre de la interface a ILoad, las interfaces deben comenzar su nombre con I por convencion. 
     public interface Load
     {
          string[] load(String filename);
